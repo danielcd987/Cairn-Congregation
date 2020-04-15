f743ee56e5513e2e0be995c157ecebe0a7092fcf
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-04-15 18:11:29 -0400

    Update profile.php

--- Cairn Congregation/Cairn Congregation/profile.php
+++ Cairn Congregation/Cairn Congregation/profile.php
@@ -28,7 +28,7 @@ include "includes/dbh.inc.php"; //includes DB connection
 
             echo'
             <form action = "includes/status.inc.php" method = "post">
-            <input name = "id_user" value = "<?php echo {$_SESSION['userId']}; ?>" type = "hidden">
+            <input name = "id_user" value = "'.$_SESSION['userId'].'"  type = "hidden">
             <input type = "text" name = "userstatus" placeholder = "How are you feeling?">
             <button type = "submit" name = "status">Update Status</button>
             </form> <br>';
