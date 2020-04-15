802050fc6e2dd0ca47f6c102a26ddd2c08a6e297
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-04-15 18:00:23 -0400

    Update status.php

--- Cairn Congregation/Cairn Congregation/status.php
+++ Cairn Congregation/Cairn Congregation/status.php
@@ -4,7 +4,7 @@ require "header.php"; //gets header navigation
 include "includes/dbh.inc.php"; //includes DB connection
 ?>
     <form action = "includes/status.inc.php" method = "post">
-            <input name = "id_user" value = "<?php echo $_SESSION['userId']; ?>" hidden>
+            <input name = "id_user" value = "<?php echo $_SESSION['userId']; ?>" type = "hidden">
             <input type = "text" name = "userstatus" placeholder = "How are you feeling?">
             <button type = "submit" name = "status">Update Status</button>
     </form> <br>
