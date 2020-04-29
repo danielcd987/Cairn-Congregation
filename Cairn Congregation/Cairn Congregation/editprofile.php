24a55ba2b23bc1f4afc8c18c54b0dd49c632acaf
Daniel DeCarlo <44644735+danielcd987@users.noreply.github.com>
2020-04-29 16:32:35 -0400

    Update editprofile.php

--- Cairn Congregation/Cairn Congregation/editprofile.php
+++ Cairn Congregation/Cairn Congregation/editprofile.php
@@ -43,8 +43,8 @@ include "includes/dbh.inc.php"; //includes DB connection
               <input name = "id_user" placeholder = "ID Number" id = "IdNum" value = "<?php echo $_SESSION['userId']; ?>" hidden >  <br> <br>
              <p>Username:</p><input type = "text" name = "userid" placeholder = "UserName" id = "user" value = "<?php echo $_SESSION['userUid']; ?>"> <br> <br>
              <p>Email:</p><input type = "text" name = "email" placeholder = "Email" id = "email" value = "<?php echo $_SESSION['email']; ?>"> <br> <br>
-             <p>Password:</p><input type = "password" name = "pswd" placeholder = "Password"> <br> <br>
-             <p>Retype Password:</p><input type = "password" name = "pswd2" placeholder = "Re-enter Password"> <br> <br>
+             <p>Password:</p><input type = "password" name = "pswd"> <br> <br>
+             <p>Retype Password:</p><input type = "password" name = "pswd2" > <br> <br>
              <p>Phone Number:</p><input type = "tel" name = "phonenumber" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" placeholder = "Enter Phone Number" value = "<?php echo  $_SESSION['phonenumber']; ?>" > <br> <br>
              <p>Occupation:</p><input type = "text" name = "occupation" placeholder = "Occupation" id = "occ" value = "<?php echo  $_SESSION['occupation']; ?>"> <br> <br>
              <p>Age:</p><input type = "number" name = "age" placeholder = "Age" id = "age" value = "<?php echo  $_SESSION['ages']; ?>" > <br> <br>
